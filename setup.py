f1d664866d986edaf7dc7181c01877ebe5bdf405
Rolf Erik Lekang <me@rolflekang.com>
2015-04-28 15:12:33 +0200

    Add initial structure

---
+++ setup.py
@@ -0,0 +1,32 @@
+# -*- encoding: utf8 -*-
+from setuptools import find_packages, setup
+
+import frigg_settings
+
+
+def _read_long_description():
+    try:
+        import pypandoc
+        return pypandoc.convert('README.md', 'rst', format='markdown')
+    except Exception:
+        return None
+
+version = frigg_settings.__version__
+
+
+setup(
+    name='frigg-settings',
+    version=version,
+    description='A module for parsing and discovery of frigg settings file',
+    long_description=_read_long_description(),
+    packages=find_packages(exclude='tests'),
+    author='The frigg team',
+    author_email='hi@frigg.io',
+    license='MIT',
+    url='https://github.com/frigg/frigg-settings',
+    py_modules=['frigg_test_discovery'],
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ]
+)
