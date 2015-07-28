72160b29f3faf013afaa4b23184faaa56453aef0
Rolf Erik Lekang <me@rolflekang.com>
2015-07-28 12:30:16 +0200

    1.0.1

--- frigg_settings/__init__.py
+++ frigg_settings/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 from frigg_settings.helpers import FileSystemWrapper  # noqa
 from frigg_settings.settings import build_tasks, load_settings_file, build_settings  # noqa
